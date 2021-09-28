86616be168d95531e9b9fc29d77fe0d6b4e3fd65
Artemy Pogosov <artemypogosov@gmail.com>
2021-09-28 23:54:02 +0300

    Update doom-emacs config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,3 +1,5 @@
+;;; Package -- summary
+;;; Commentary:
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
@@ -6,6 +8,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
+;;; Code:
 (setq user-full-name "Artemy Pogosov"
       user-mail-address "artemypogosov@gmail.com")
 
@@ -25,18 +28,21 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(load-theme 'doom-gruvbox)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
+(defvar org-directory)
 (setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
+(defvar display-line-numbers-type)
 (setq display-line-numbers-type t)
 
 
 ;; PROJECTILE
+(defvar projectile-project-search-path)
 (setq projectile-project-search-path '("~/IdeaProjects"))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
@@ -55,3 +61,5 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;
+;;; config.el ends here
