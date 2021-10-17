7dbaa69f4e3509bdcb0c8cf00e49acb8c324d189
Artemy <artemypogosov@gmail.com>
2021-10-18 00:06:58 +0300

    Change xmobar battery icons; Add dunst

--- xmonad/scripts/autostart.sh
+++ xmonad/scripts/autostart.sh
@@ -14,6 +14,7 @@ xsetroot -cursor_name left_ptr &
 # Starting applications at boot time
 nitrogen --restore &
 #powerkit &
+dunst
 lxqt-powermanagement &
 trayer --edge bottom --align right --widthtype request --padding 5 --SetDockType true --SetPartialStrut false --expand true --monitor 1 --transparent true --alpha 256 --height 20 &
 nm-applet &
