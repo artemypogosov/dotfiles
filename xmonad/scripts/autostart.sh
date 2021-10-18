0c2781ec9e1c25067eaacb3ecf504b07d9a92277
Artemy <artemypogosov@gmail.com>
2021-10-18 14:19:35 +0300

    Add udiskie to xmonad autostart script

--- xmonad/scripts/autostart.sh
+++ xmonad/scripts/autostart.sh
@@ -14,7 +14,8 @@ xsetroot -cursor_name left_ptr &
 # Starting applications at boot time
 nitrogen --restore &
 #powerkit &
-dunst
+dunst &
+udiskie &
 lxqt-powermanagement &
 trayer --edge bottom --align right --widthtype request --padding 5 --SetDockType true --SetPartialStrut false --expand true --monitor 1 --transparent true --alpha 256 --height 20 &
 nm-applet &
